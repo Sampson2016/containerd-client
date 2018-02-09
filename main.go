0bbcfc875796b4e36835a4c903f749466e41523b
Sunjay Bhatia <sbhatia@pivotal.io>
2018-02-09 12:08:33 -0500

    create volume with winc-image and pass spec to containerd
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/cio"
 	"github.com/containerd/containerd/namespaces"
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 )
+
+const rootfs = "C:\\ProgramData\\docker\\windowsfilter\\af4b828a79120f7c72f60f10f1b359fcd90ff70432cd1536b155a6814526b69e"
 
 func main() {
 	client, err := containerd.New(`\\.\pipe\containerd-containerd`)
@@ -18,11 +22,20 @@ func main() {
 		panic(err)
 	}
 
+	containerId := os.Args[1]
+
 	ctx := namespaces.WithNamespace(context.Background(), "default")
 
-	bundleSpec := &specs.Spec{}
+	bundleSpec, err := generateSpec(containerId)
+	if err != nil {
+		panic(err)
+	}
+	bundleSpec.Process = &specs.Process{
+		Cwd:  "C:\\",
+		Args: []string{"cmd.exe"},
+	}
 
-	container, err := client.NewContainer(ctx, "test2-winc", containerd.WithRuntime("io.containerd.runtime.v1.winc", nil), containerd.WithSpec(bundleSpec))
+	container, err := client.NewContainer(ctx, containerId, containerd.WithRuntime("io.containerd.runtime.v1.winc", nil), containerd.WithSpec(bundleSpec))
 	if err != nil {
 		panic(err)
 	}
@@ -42,4 +55,23 @@ func main() {
 	for _, c := range containers {
 		fmt.Printf("container: %+v\n", c)
 	}
+
+}
+
+func generateSpec(containerId string) (*specs.Spec, error) {
+	wincImageBinary := `c:\workspace\winc-image-binary\winc-image.exe`
+	exec.Command(wincImageBinary, "delete", containerId).Run()
+
+	o, err := exec.Command(wincImageBinary, "create", rootfs, containerId).CombinedOutput()
+	if err != nil {
+		fmt.Println(string(o))
+		return nil, err
+	}
+
+	var b specs.Spec
+	if err := json.Unmarshal(o, &b); err != nil {
+		return nil, err
+	}
+
+	return &b, nil
 }
